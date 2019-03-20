3d398953d35fb8fd2b6a19ade3e4cdc3828c1bcd
tahannejad <m.m.tahannejad@gmail.com>
2019-03-21 02:43:52 +0330

    Updates

---
+++ precache-manifest.3c4cb746b21326627f07e998cba63511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5161e5661c54c6386363",
+    "url": "/my-resume/static/css/main.9805f51e.chunk.css"
+  },
+  {
+    "revision": "5161e5661c54c6386363",
+    "url": "/my-resume/static/js/main.2e27c3cb.chunk.js"
+  },
+  {
+    "revision": "2f7b259ab1933d650523",
+    "url": "/my-resume/static/js/runtime~main.1423441b.js"
+  },
+  {
+    "revision": "987f66a8e80f74c79fd8",
+    "url": "/my-resume/static/js/2.734b344a.chunk.js"
+  },
+  {
+    "revision": "5a09dcca5ce68b5573766d6f9e91b27b",
+    "url": "/my-resume/index.html"
+  }
+];
